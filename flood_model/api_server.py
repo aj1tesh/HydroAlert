91812a02889665d39a019dc535bef0722ee2fa6b
Ajitesh <singh.ajitesh1449@gmail.com>
2026-04-23 21:20:52 +0530

    Cors fixes

--- flood_model/api_server.py
+++ flood_model/api_server.py
@@ -18,6 +18,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "https://hydroalert-ivory.vercel.app/",
         "http://localhost:3000",
         "http://localhost:5173",
         "http://127.0.0.1:3000",
